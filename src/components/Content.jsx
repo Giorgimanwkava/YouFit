3b33d07d2b3813dc9eafac6b1f569d04a369da5b
Giorgi manwkava <manwkavagiorgi@gmail.com>
2024-04-09 17:57:09 +0400

    added search bar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import './Content.css'
 
 const Content = () => {
+  // const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=1';
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': '41439c410emsh69908f56a5b982dp1f7bc8jsn1bba7960eb67',
+  //     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  //   }
+  // };
+  // // async function fetchData() {
+  // //   try {
+  // //     const response = await fetch(url, options);
+  // //     const result = await response.json();
+  // //     console.log(result);
+  // //   } catch (error) {
+  // //     console.error(error);
+  // //   }
+  // // }
+  // // fetchData();
+  // fetch(url,options)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data[0]))
+  //   .catch(error => console.log(error))
   return (
-    <div>
-      Content
-    </div>
+    <section className='main-content'>
+      <h1>Search for excercises</h1>
+      <div className='searchbar-and-btn'>
+        <input className='search-bar' type="text" placeholder="Search..." />
+        <button className='search-btn'>Search</button>
+      </div>
+    </section>
   )
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,11 +3,11 @@ import './LandingPage.css'
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
+    <section className="landing-page">
       <h1 className='catchphrase'>Don't Worry, <br/> &nbsp;&nbsp;&nbsp;&nbsp; Be Healthy</h1>
       <h4 className='website-description'>Make up exercise routine that fits YOU!</h4>
       <button className='explore-exercise-btn'>Explore exercises</button>
-    </div>
+    </section>
   )
 }
 
