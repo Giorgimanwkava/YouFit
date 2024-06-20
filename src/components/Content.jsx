90ea861c5d593d3345f6f30ade659695cecb066b
Giorgi manwkava <manwkavagiorgi@gmail.com>
2024-06-20 15:50:07 +0400

    added filtering

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -36,17 +36,50 @@ const Content = () => {
     //     fetchData(offset);
     // },[]) 
   
-  const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState(Data);
+  const [paginatedExercises, setPaginatedExercises] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const exercisesPerPage = 6;
-  const totalPages = Math.ceil(Data.length / exercisesPerPage);
+
+  const [bodyPartFilter, setBodyPartFilter] = useState('');
+  const [equipmentFilter, setEquipmentFilter] = useState('');
+  const [targetFilter, setTargetFilter] = useState('');
+  const [filteredExercises, setFilteredExercises] = useState(exercises);
+  
   const exercisesContainerRef = useRef(null);
 
+  const exercisesPerPage = 6;
+  const totalPages = Math.ceil(filteredExercises.length / exercisesPerPage);
+
+  const handleBodyPartChange = (e) => {
+    setBodyPartFilter(e.target.value);
+  };
+  
+  const handleEquipmentChange = (e) => {
+    setEquipmentFilter(e.target.value);
+  };
+  
+  const handleTargetChange = (e) => {
+    setTargetFilter(e.target.value);
+  };
+
+  // filtering exercises based on multi-select
+  useEffect(() => {
+    const filteredData = exercises.filter(exercise => {
+      return (
+        (bodyPartFilter === '' || exercise.bodyPart === bodyPartFilter) &&
+        (equipmentFilter === '' || exercise.equipment === equipmentFilter) &&
+        (targetFilter === '' || exercise.target === targetFilter)
+      );
+    });
+    setFilteredExercises(filteredData);
+  }, [bodyPartFilter, equipmentFilter, targetFilter]);
+
+  // displaying paginated exercises
   useEffect(() => {
     const offset = (currentPage - 1) * exercisesPerPage;
-    const paginatedData = Data.slice(offset, offset + exercisesPerPage);
-    setExercises(paginatedData);
-  }, [currentPage]);
+    const paginatedData = filteredExercises.slice(offset, offset + exercisesPerPage);
+    setPaginatedExercises(paginatedData);
+  }, [currentPage, filteredExercises]);
 
   const handlePageClick = (pageNumber) => {
     if (pageNumber !== currentPage) {
@@ -85,8 +118,20 @@ const Content = () => {
         <button className="fa-solid fa-magnifying-glass search-btn"></button>
       </div>
       <div className='multi-select'>
-        <select name='bodypart' id='bodypart-select'>
+        <select name='bodypart' id='bodypart-select' value={bodyPartFilter} onChange={handleBodyPartChange}>
           <option value="">Choose bodypart</option>
+          <option value="back">back</option>
+          <option value="cardio">cardio</option>
+          <option value="chest">chest</option>
+          <option value="lower arms">lower arms</option>
+          <option value="lower legs">lower legs</option>
+          <option value="neck">neck</option>
+          <option value="shoulders">shoulders</option>
+          <option value="upper arms">upper arms</option>
+          <option value="upper legs">upper legs</option>
+          <option value="waist">waist</option>
+
+          {/* <option value="">Choose bodypart</option>
           <option value="back">back</option>
           <option value="cardio">cardio</option>
           <option value="chest">chest</option>
@@ -96,10 +141,40 @@ const Content = () => {
           <option value="shoulders">shoulders</option>
           <option value="upper%20arms">upper arms</option>
           <option value="upper%20legs">upper legs</option>
-          <option value="waist">waist</option>
+          <option value="waist">waist</option> */}
         </select>
-        <select name="equipment" id="equipment-select">
+        <select name="equipment" id="equipment-select" value={equipmentFilter} onChange={handleEquipmentChange}>
           <option value="">Choose equipment</option>
+          <option value="assisted">assisted</option>
+          <option value="band">band</option>
+          <option value="barbell">barbell</option>
+          <option value="body weight">body weight</option>
+          <option value="bosu ball">bosu ball</option>
+          <option value="cable">cable</option>
+          <option value="dumbbell">dumbbell</option>
+          <option value="elliptical machine">elliptical machine</option>
+          <option value="ez barbell">ez barbell</option>
+          <option value="hammer">hammer</option>
+          <option value="kettlebell">kettlebell</option>
+          <option value="leverage machine">leverage machine</option>
+          <option value="medicine ball">medicine ball</option>
+          <option value="olympic ball">olympic ball</option>
+          <option value="resistance band">resistance band</option>
+          <option value="roller">roller</option>
+          <option value="rope">rope</option>
+          <option value="skierg machine">skierg machine</option>
+          <option value="sled machine">sled machine</option>
+          <option value="smith machine">smith machine</option>
+          <option value="stability ball">stability ball</option>
+          <option value="stationary bike">stationary bike</option>
+          <option value="stepmill machine">stepmill machine</option>
+          <option value="tire">tire</option>
+          <option value="trap bar">trap bar</option>
+          <option value="upper body ergometer">upper body ergometer</option>
+          <option value="weighted">weighted</option>
+          <option value="wheel roller">wheel roller</option>
+
+          {/* <option value="">Choose equipment</option>
           <option value="assisted">assisted</option>
           <option value="band">band</option>
           <option value="barbell">barbell</option>
@@ -127,10 +202,30 @@ const Content = () => {
           <option value="trap%20bar">trap bar</option>
           <option value="upper%20body%20ergometer">upper body ergometer</option>
           <option value="weighted">weighted</option>
-          <option value="wheel%20roller">wheel roller</option>
+          <option value="wheel%20roller">wheel roller</option> */}
         </select>
-        <select name='target' id='target-select'>
+        <select name='target' id='target-select' value={targetFilter} onChange={handleTargetChange}>
           <option value="">Choose target muscle</option>
+          <option value="abductors">abductors</option>
+          <option value="abs">abs</option>
+          <option value="adductors">adductors</option>
+          <option value="biceps">biceps</option>
+          <option value="calves">calves</option>
+          <option value="cardiovascular system">cardiovascular system</option>
+          <option value="forearms">forearms</option>
+          <option value="glutes">glutes</option>
+          <option value="hamstrings">hamstrings</option>
+          <option value="lats">lats</option>
+          <option value="levator scapulae">levator scapulae</option>
+          <option value="pectorals">pectorals</option>
+          <option value="quads">quads</option>
+          <option value="serratus anterior">serratus anterior</option>
+          <option value="spine">spine</option>
+          <option value="traps">traps</option>
+          <option value="triceps">triceps</option>
+          <option value="upper back">upper back</option>
+
+          {/* <option value="">Choose target muscle</option>
           <option value="abductors">abductors</option>
           <option value="abs">abs</option>
           <option value="adductors">adductors</option>
@@ -148,24 +243,23 @@ const Content = () => {
           <option value="spine">spine</option>
           <option value="traps">traps</option>
           <option value="triceps">triceps</option>
-          <option value="upper%20back">upper back</option>
+          <option value="upper%20back">upper back</option> */}
         </select>
       </div>
-      <div id="exercises-container" ref={exercisesContainerRef} className='exercises'>
-        {exercises.map((exercise, index) => (
+      <div ref={exercisesContainerRef} className='exercises'>
+        {paginatedExercises.map((exercise, index) => (
           <ExcerciseCard key={index} exercise={exercise} />
         ))}
       </div>
       <div className='pagination'>
         <button
-          className='page-number'
+          className={`page-number ${1 === currentPage ? 'disabled' : ''}`}
           onClick={() => handlePageClick(currentPage - 1)}
-          disabled={currentPage === 1}
         >
           &lt;
         </button>
         <button
-          className='page-number'
+          className={`page-number ${1 === currentPage ? 'disabled' : ''}`}
           onClick={() => handlePageClick(1)}
         >
           1
@@ -173,7 +267,7 @@ const Content = () => {
         {currentPage > 3 && <span className='skipped-page-dots'>...</span>}
         {getPaginationNumbers().map((pageNumber) => (
           <button
-            className='page-number'
+            className={`page-number ${pageNumber === currentPage ? 'disabled' : ''}`}
             key={pageNumber}
             onClick={() => handlePageClick(pageNumber)}
           >
@@ -181,16 +275,15 @@ const Content = () => {
           </button>
         ))}
         {currentPage < totalPages - 2 && <span className='skipped-page-dots'>...</span>}
-        <button
-          className='page-number'
+        {totalPages > 1 && <button
+          className={`page-number ${totalPages === currentPage ? 'disabled' : ''}`}
           onClick={() => handlePageClick(totalPages)}
         >
           {totalPages}
-        </button>
+        </button>}
         <button
-          className='page-number'
+          className={`page-number ${totalPages === currentPage ? 'disabled' : ''}`}
           onClick={() => handlePageClick(currentPage + 1)}
-          disabled={currentPage === totalPages}
         >
           &gt;
         </button>
